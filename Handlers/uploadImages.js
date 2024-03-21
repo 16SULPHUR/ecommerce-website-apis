6ce15b220f0fa8a4eb6e01802eace89851e2caa2
“Ankit <22amtics124@gmail.com>
2024-03-21 22:28:06 +0530

    used imagekit

--- Handlers/uploadImages.js
+++ Handlers/uploadImages.js
@@ -1,15 +1,40 @@
+var ImageKit = require("imagekit");
+
+var imagekit = new ImageKit({
+  publicKey: "public_gkPbyKgZlAzLEh+N4QjuH1lJzYo=",
+  privateKey: "private_KLXMquDly6NZvTLUjtAA0mYFRss=",
+  urlEndpoint: "https://ik.imagekit.io/dqn1rnabh/",
+});
+
 const uploadImage = async (image) => {
-  const { uploadFile } = await import("@uploadcare/upload-client");
+  const file = imagekit.upload(
+    {
+      file: image.buffer, //required
+      fileName: "my_file_name.jpg", //required
+      tags: ["tag1", "tag2"],
+    },
+    function (error, result) {
+      if (error) console.log(error);
+      else console.log(result);
+    }
+  );
 
-  const file = await uploadFile(image.buffer, {
-    publicKey: "1bf395a5c5b0e02359fe",
-    fileName: image.originalFileName,
-    contentType: "image/jpeg",
-  });
-
-  console.log(file.cdnUrl);
-  return file.cdnUrl;
+  console.log(file.url);
+  return file.url;
 };
+
+// const uploadImage = async (image) => {
+//   const { uploadFile } = await import("@uploadcare/upload-client");
+
+//   const file = await uploadFile(image.buffer, {
+//     publicKey: "1bf395a5c5b0e02359fe",
+//     fileName: image.originalFileName,
+//     contentType: "image/jpeg",
+//   });
+
+//   console.log(file.cdnUrl);
+//   return file.cdnUrl;
+// };
 
 const uploadImages = async (images) => {
   let cdnURLs = [];
@@ -23,8 +48,8 @@ const uploadImages = async (images) => {
 };
 
 const uploadThumbnail = async (thumbnail) => {
-    const thumbnailURL = await uploadImage(thumbnail)
-    return thumbnailURL
+  const thumbnailURL = await uploadImage(thumbnail);
+  return thumbnailURL;
 };
 
-module.exports = {uploadImages, uploadThumbnail};
+module.exports = { uploadImages, uploadThumbnail };
