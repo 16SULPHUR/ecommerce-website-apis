b0de260c5f0e44f6b40649119baa835dd5548e86
“Ankit <22amtics124@gmail.com>
2024-03-22 09:22:52 +0530

    cors error

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 
 mongoose
