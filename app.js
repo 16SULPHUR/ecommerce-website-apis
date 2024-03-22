b57a49d377f149962e88737894b661a17e0b3d91
“Ankit <22amtics124@gmail.com>
2024-03-22 09:44:43 +0530

    set cors header

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.use(cors());
 //   next();
 // })
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 
 mongoose
