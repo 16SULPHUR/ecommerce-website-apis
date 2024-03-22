9f1dfd3e969be50219b61423ec381981b827fce7
“Ankit <22amtics124@gmail.com>
2024-03-22 10:01:18 +0530

    cors commented

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.use(cors());
+// app.use(cors());
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
